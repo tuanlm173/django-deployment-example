fe4e0735442b32b7b2a0bdadab576288455642f4
Tuan Le <tuanlm.cca@gmail.com>
2019-10-08 16:54:27 +0700

    add allowed host test

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=@mgxkqw74!kb6e%jevk64!2=j%a%g_0m%*ky^n5#(ei*oe4cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tuanlm173.pythonanywhere.com']
 
 
 # Application definition
