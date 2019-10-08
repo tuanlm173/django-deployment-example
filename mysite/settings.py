1045d82ba561bdd88c0639f47b62abd72c97f132
Tuan Le <tuanlm.cca@gmail.com>
2019-10-08 18:05:08 +0700

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '=@mgxkqw74!kb6e%jevk64!2=j%a%g_0m%*ky^n5#(ei*oe4cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']  # ['tuanlm173.pythonanywhere.com']
+ALLOWED_HOSTS = ['tuanlepersonalweb.herokuapp.com']  # ['tuanlm173.pythonanywhere.com']
 
 # Application definition
 
