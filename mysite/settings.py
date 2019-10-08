0480fa9755153c11e116078a6e9fd8a9d775f1cd
Tuan Le <tuanlm.cca@gmail.com>
2019-10-08 17:45:29 +0700

    change setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 import dj_database_url
 
